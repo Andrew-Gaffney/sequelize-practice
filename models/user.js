2644d9df4b46ae9f8d9921e2cfb202ea7b590e31
andrew.atreides@gmail.com <andrew.atreides@gmail.com>
2017-05-07 14:47:36 -0400

    Completed Sequelize practice excercises.

--- models/user.js
+++ models/user.js
@@ -3,8 +3,42 @@ const db = new Sequelize('postgres://localhost:5432/sequelize_practice');
 
 const User = db.define('user', {
 // YOUR CODE HERE...
+
+first: Sequelize.STRING,
+
+last: Sequelize.STRING,
+
+age: {
+  type: Sequelize.INTEGER,
+  validate: {
+    min: 18
+  }
+},
+
+email: {
+  type: Sequelize.STRING,
+  allowNull: false
+},
+
+bio: Sequelize.TEXT
+
 }, {
 // ...AND HERE
-});
+getterMethods: {
+  fullName: function(){
+    return this.getDataValue('first') + ' ' + this.getDataValue('last');
+  }
+},
 
+instanceMethods: {
+
+  haveBirthday: function() {
+    return new Sequelize.Promise((resolve, reject) => {
+      this.setDataValue('age', this.getDataValue('age') + 1)
+      resolve(this.save());
+    });
+  }
+}
+
+});
 module.exports = User;
